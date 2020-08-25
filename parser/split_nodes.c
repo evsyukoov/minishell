6ac789f2dc8f2486611eb0f3abce23e8d89ac9f6
Casie Carl <ccarl@mi-s5.msk.21-school.ru>
2020-08-25 15:07:20 +0300

    ; and | works correct

--- parser/split_nodes.c
+++ parser/split_nodes.c
@@ -3,50 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: denis <denis@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ccarl <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/21 18:10:41 by ccarl             #+#    #+#             */
-/*   Updated: 2020/08/25 01:07:53 by denis            ###   ########.fr       */
+/*   Created: 2020/08/25 15:00:38 by ccarl             #+#    #+#             */
+/*   Updated: 2020/08/25 15:00:38 by ccarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../include/minishell.h"
+#include "../include/minishell.h"
 
-char	quote_type(char *arg)
+int 	parse_error(char type)
 {
-	if (*arg == '\"')
-		return ('\"');
-	else if (*arg == '\'')
-		return ('\'');
-	else
-		return ('\0');
+	//parse_syntax_error(flag);
+	return (-1);
 }
 
-static void		skip_d(char const **s, char delimetr)
-{
-	while (**s == delimetr && **s)
-		(*s)++;
-}
-
-static int		word_len(char const *s, char delimetr)
+int 	skip_quotes(char **s, char q_type)
 {
 	int len;
 
 	len = 0;
-	while (s[len] != delimetr && s[len])
-		len++;  
-	return (len);
-}
-
-void 	skip_quotes(char **s, char q_type)
-{
 	if (q_type)
 	{
 		(*s)++;
+		len++;
 		while (**s && **s != q_type)
+		{
 			(*s)++;
+			len++;
+		}
 		(*s)++;
+		len++;
 	}
+	return (len);
+}
+
+int		word_len(char *s, char delimetr)
+{
+	int len;
+	char q_type;
+
+	len = 0;
+	while (*s && *s != delimetr)
+	{
+		q_type = quote_type(s);
+		while (q_type)
+		{
+			len += skip_quotes(&s, q_type);
+			q_type = quote_type(s);
+		}
+		if (*s == delimetr)
+			return (len);
+		s++;
+		len++;
+	}
+	return (len);
 }
 
 int		word_counter(char *s, char delimetr)
@@ -56,64 +67,53 @@ int		word_counter(char *s, char delimetr)
 
 	words = 0;
 	if (*s == delimetr)
-	{
-	//	parse_syntax_error(5);
-		return (-1);
-	}
+		return (parse_error(*s));
 	while (*s)
 	{
 		q_type = quote_type(s);
-		if (q_type)
+		while (q_type)
 		{
 			skip_quotes(&s, q_type);
-			s++;
+			q_type = quote_type(s);
 		}
-		printf("s = %s\n", s);
 		if (*s == delimetr)
 		{
-			s++;
+			if (*(s + 1) == delimetr)
+				return (parse_error(q_type));
 			words++;
 		}
-		else if (!*(s + 1))
+		else if(!*(s + 1))
 			words++;
 		s++;
 	}
 	return (words);
 }
 
-/*char			**ft_split(char const *s, char c)
+char			**split(char *s, char c)
 {
 	int		words;
 	int		i;
 	int		j;
 	char	**res;
+	int 	len;
 
-	if (!s)
+	if ((words = word_counter(s, c)) == -1)
 		return (0);
-	words = word_counter(s, c);
 	if (!(res = (char**)malloc(sizeof(char*) * (words + 1))))
 		return (NULL);
 	i = 0;
-	skip(&s, c);
 	while (i < words)
 	{
-		if (!(res[i] = (char*)malloc(sizeof(char) * (word_len(s, c) + 1))))
+		len = word_len(s, c);
+		if (!(res[i] = (char*)malloc(sizeof(char) * (len + 1))))
 			return (0);
 		j = 0;
-		while (*s != c && *s)
+		while (j < len)
 			res[i][j++] = *s++;
 		res[i][j] = '\0';
 		i++;
-		skip(&s, c);
+		s++;
 	}
 	res[i] = NULL;
 	return (res);
-}*/
-
-int 	main(void)
-{
-	char test[1000] = "ascasfsa\"aefqfeqq\"";
-	printf("res = %d\n", word_counter(test, ';'));
 }
-
-
